5036e27f9d60881d63ba755307c22f3ae6e9f4a8
davidplatino <dsplatino@gmail.com>
2023-01-22 18:35:50 -0300

    Implementação das rotas Delete, Show e Update

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -7,6 +7,16 @@ const usersController = new UsersController();
 
 usersRouter.get('/', usersController.index);
 
+usersRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  usersController.show,
+);
+
 usersRouter.post(
   '/',
   celebrate({
@@ -17,6 +27,31 @@ usersRouter.post(
     },
   }),
   usersController.create,
+);
+
+usersRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  usersController.update,
+);
+
+usersRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  usersController.delete,
 );
 
 export default usersRouter;
